1027effc8ffb4fdc12a1239f59302eedffbcf447
jinyoung1018 <jywsb1816@gmail.com>
2024-02-15 21:43:32 +0900

    리뷰 목록 api 연동

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,9 +1,12 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import axios from "@/lib/axios";
+import SizeReviewList from "@/components/SizeReviewList";
 
 export default function Product() {
   const [product, setProduct] = useState();
+  const [sizeReviews, setSizeReviews] = useState([]);
+
   const router = useRouter();
   const { id } = router.query;
 
@@ -13,17 +16,26 @@ export default function Product() {
     setProduct(nextProduct);
   }
 
+  async function getSizeReviews(targetId) {
+    const res = await axios.get(`/size_reviews/?product_id=${targetId}`);
+    const nextSizeReviews = res.data.results ?? [];
+    setSizeReviews(nextSizeReviews);
+  }
+
   useEffect(() => {
     if (!id) return;
     getProduct(id);
+    getSizeReviews(id);
   }, [id]);
 
   if (!product) return null;
+  console.log(sizeReviews);
 
   return (
     <div>
       <h1>{product.name}</h1>
       <img src={product.imgUrl} alt={product.name}></img>
+      <SizeReviewList sizeReviews={sizeReviews}></SizeReviewList>
     </div>
   );
 }
